1cc33bfece56204e979faffc60d62c6754b8d139
TyTerror <tyler.elias.ramsey@gmail.com>
2020-12-13 17:36:46 -0500

    Update shell.c

--- project-1--create-a-simple-shell/shell.c
+++ project-1--create-a-simple-shell/shell.c
@@ -7,7 +7,6 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
-
 #define MAX_COMMAND_LINE_LEN 1024
 #define MAX_COMMAND_LINE_ARGS 128
 
@@ -18,10 +17,12 @@ extern char **environ;
 char *getcwd(char *buf, size_t size);
 
 char cwd[100];
-void siquit();
+void sighandler(int);
+//void siquit();
+pid_t pid;
 
 int main() {
-
+    signal(SIGINT, sighandler);
     // Stores the string typed into the command line.
     char command_line[MAX_COMMAND_LINE_LEN];
     char cmd_bak[MAX_COMMAND_LINE_LEN];
@@ -30,7 +31,11 @@ int main() {
     char *arguments[MAX_COMMAND_LINE_ARGS];
     int i = 0;
     int j = 0;
+    int k = 0;
+    bool isBackground = false;
+  
     while (true) {
+      isBackground = false;
       
         do{ 
          
@@ -78,18 +83,78 @@ int main() {
         while(arguments[i] != NULL){
           arguments[++i] = strtok(NULL, delimiters);
         }
-        for(j = 0; j<i; j++){
-          printf("%s\n",arguments[j]);
-        }
+        arguments[i] == NULL;
+        //for(j = 0; j<i; j++){
+         // printf("%s\n",arguments[j]);
+        //}
         // 2. Implement Built-In Commands
+        
       
-    
+      
+      
+      
+      
+      
+        if(strcmp(arguments[0], "cd")==0){
+          
+          chdir(arguments[1]);
+        }
+        else if(strcmp(arguments[0], "pwd")==0){
+          if(getcwd(cwd, sizeof(cwd)) == NULL){
+            perror("getcwd() error");
+            return 1;
+          }
+          
+          printf("%s\n", cwd);
+          fflush(stdout);
+        }
+        else if (strcmp(arguments[0], "echo")==0){
+          k = 1;
+          while(arguments[k]!=NULL){
+            printf("%s",arguments[k]);
+            k++;
+          }
+          printf("\n"); 
+          //printf("echo function\n");
+        }
+        else if (strcmp(arguments[0], "exit")==0){
+          exit(0);
+        }
+        else if (strcmp(arguments[0], "env")==0){
+          //getenv(arguments[1])
+          printf("%s\n",getenv(arguments[1]));
+        }
+        else if (strcmp(arguments[0], "setenv")==0){
+          
+          printf("setenv function\n");
+        }
+        else {
+          if (strcmp(arguments[i-1], "&") == 0){
+            arguments[i-1] = NULL;
+            isBackground = true;
+          }
+          
+          
+          pid = fork();
+          if (pid < 0){
+            perror("fork failed");
+            exit(0);
+          }
+          else if (pid == 0){
+            
+            execvp(arguments[0], arguments);
+            
+            
+            exit(0);
+          }
+          else{// 4. The parent process should wait for the child to complete unless its a background process
+            
+            if(isBackground != true){
+              wait(NULL);
+            }
+          }
+        }
         // 3. Create a child process which will execute the command line input
-
-  
-        // 4. The parent process should wait for the child to complete unless its a background process
-      
-      
         // Hints (put these into Google):
         // man fork
         // man execvp
@@ -97,7 +162,6 @@ int main() {
         // man strtok
         // man environ
         // man signals
-        
         // Extra Credit
         // man dup2
         // man open
@@ -105,4 +169,10 @@ int main() {
     }
     // This should never be reached.
     return -1;
+}
+
+
+void sighandler(signum){
+  printf("Caught signal %d, coming out...\n", signum);
+  kill(pid, SIGKILL);
 }
