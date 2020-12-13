2fd8a1de91b27c7c7a53ee88896e3d8d8fe2b82e
TyTerror <tyler.elias.ramsey@gmail.com>
2020-12-13 13:18:01 -0500

    Update shell.c

--- project-1--create-a-simple-shell/shell.c
+++ project-1--create-a-simple-shell/shell.c
@@ -4,34 +4,58 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
 
 #define MAX_COMMAND_LINE_LEN 1024
 #define MAX_COMMAND_LINE_ARGS 128
 
-char prompt[] = "shell > ";
+char prompt[] = "> ";
 char delimiters[] = " \t\r\n";
-char **environ;
+extern char **environ;
+
+char *getcwd(char *buf, size_t size);
+
+char cwd[100];
+void siquit();
 
 int main() {
+
     // Stores the string typed into the command line.
     char command_line[MAX_COMMAND_LINE_LEN];
-
+    char cmd_bak[MAX_COMMAND_LINE_LEN];
+  
     // Stores the tokenized command line input.
     char *arguments[MAX_COMMAND_LINE_ARGS];
-    
+    int i = 0;
+    int j = 0;
     while (true) {
-        // Print the shell prompt.
-        printf("%s", prompt);
-        fflush(stdout);
-        
-        // Read input from stdin and store it in command_line. If there's an
-        // error, exit immediately. (If you want to learn more about this line,
-        // you can Google "man fgets")
-        if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
-            fprintf(stderr, "fgets error");
-            exit(0);
-        }
-        
+      
+        do{ 
+         
+          
+          
+          if (getcwd(cwd, sizeof(cwd)) != NULL){
+            //printf("%s>", cwd);
+          }else{
+            perror("getcwd() error");
+            return 1;
+          }
+            // Print the current prompt.
+            printf("%s>", cwd);
+            fflush(stdout);
+
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);}
+ 
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+
+      
         // If the user input was EOF (ctrl+d), exit the shell.
         if (feof(stdin)) {
             printf("\n");
@@ -40,35 +64,44 @@ int main() {
             return 0;
         }
 
-        // TODO:
+        // TODO: 
+        
+			  // 0. Modify the prompt to print the current working directory
+			  
+			
         // 1. Tokenize the command line input (split it on whitespace)
-         int i = 0;
-         do {
-            arguments[i] = strtok(command_line, delimiters);
-            i++;
-        }while(arguments[i] != NULL);
-        // 2. Create a child process which will execute the command line input
-        int isChildProcess = fork();
-        bool result;
-        if(execve(arguments[0], arguments, environ) == -1){
-          printf("%s command not found.\n", arguments[0]);
-          result = false;
-        } 
-        else {
-          result = true;
+
+        arguments[0] = strtok(command_line, delimiters);
+      
+      
+        i = 0;
+        while(arguments[i] != NULL){
+          arguments[++i] = strtok(NULL, delimiters);
         }
- 
-        if (isChildProcess == 0 && result == true){
-          return 0;
-        } else {
-          return -1;
+        for(j = 0; j<i; j++){
+          printf("%s\n",arguments[j]);
         }
-        // 3. The parent process should wait for the child to complete
-        if (isChildProcess > 0){
-          wait(NULL);
-        } else {
-          return -1;
-        }
+        // 2. Implement Built-In Commands
+      
+    
+        // 3. Create a child process which will execute the command line input
+
+  
+        // 4. The parent process should wait for the child to complete unless its a background process
+      
+      
+        // Hints (put these into Google):
+        // man fork
+        // man execvp
+        // man wait
+        // man strtok
+        // man environ
+        // man signals
+        
+        // Extra Credit
+        // man dup2
+        // man open
+        // man pipes
     }
     // This should never be reached.
     return -1;
